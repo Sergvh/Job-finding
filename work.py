8c03446d74d7770580c34f858331adb48fc4fba0
Serhii Hrytsenko <sergvh@gmail.com>
2019-09-08 12:02:41 +0300

    work whith work.ua complete

--- work.py
+++ work.py
@@ -15,7 +15,6 @@ jobs = []
 urls = []
 
 urls.append(base_url)
-
 req = session.get(base_url, headers=headers)
 
 if req.status_code == 200:
@@ -29,6 +28,7 @@ if req.status_code == 200:
 
 for url in urls:
     time.sleep(2)
+    req = session.get(url, headers=headers)
     if req.status_code == 200:
         bsObj = BS(req.content, "html.parser")
         div_list = bsObj.find_all('div', attrs={'class': 'job-link'})
