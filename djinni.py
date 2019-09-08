ec08039804a8d8ab949e667e00cadf272a28c569
Serhii Hrytsenko <sergvh@gmail.com>
2019-09-08 15:57:46 +0300

    djinni.co added

--- djinni.py
+++ djinni.py
@@ -8,46 +8,40 @@ headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 5.1; rv:47.0) Gecko/20100101 '
            'Firefox/47.0', 'Accept': 'text/html,application/xhtml+xml,'
            'application/xml;q=0.9,*/*;q=0.8'}
 
-base_url = 'https://www.work.ua/jobs-kyiv-python/'
+base_url = 'https://djinni.co/jobs/?lang=uk&location=%D0%9A%D0%B8%D0%B5%D0%B2&' \
+           'page=1&primary_keyword=Python'
 
-domain = 'https://www.work.ua'
+domain = 'https://djinni.co'
 jobs = []
 urls = []
 
 urls.append(base_url)
-req = session.get(base_url, headers=headers)
-
-if req.status_code == 200:
-    bsObj = BS(req.content, "html.parser")
-    pagination = bsObj.find('ul', attrs={'class': 'pager'})
-
-    if pagination:
-        pages = pagination.find_all('li', attrs={'class': False})
-        for page in pages:
-            urls.append(domain + page.a['href'])
+urls.append(base_url+'&page=2')
+urls.append(base_url+'&page=3')
 
 for url in urls:
     time.sleep(1)
     req = session.get(url, headers=headers)
     if req.status_code == 200:
         bsObj = BS(req.content, "html.parser")
-        div_list = bsObj.find_all('div', attrs={'class': 'job-link'})
-        for div in div_list:
-            title = div.find('h2')
-            href = title.a['href']
-            short = div.p.text
-            company = 'No name'
-            logo = div.find('img')
-            if logo:
-                company = logo['alt']
+        li_list = bsObj.find_all('li', attrs={'class': 'list-jobs__item'})
+        for li in li_list:
+            div = li.find('div', attrs={'class': 'list-jobs__title'})
+            title = div.a.text
+            href = div.a['href']
+            short = "No Description"
+            #company = 'No name'
+            descr = li.find('div', attrs={'class': 'list-jobs__description'})
+            if descr:
+                short = descr.p.text
             jobs.append({'href': domain + href,
-                         'title': title.text,
+                         'title': title,
                          'descript': short,
-                         'company': company})
+                         'company': "No name"})
 template = '<!DOCTYPE html> <html lang="uk"> <head> <meta charset="utf-8"> ' \
            '</head> <body>'
 end = '</body> <html>'
-content = '<h2> Work.ua </h2>'
+content = '<h2> djinni.co </h2>'
 
 for job in jobs:
     content += '<a href="{href}" target="_blank">{title}</a></br><p>{descript}' \
@@ -56,6 +50,6 @@ for job in jobs:
 
 data = template + content + end
 
-handle = codecs.open('urls.html', "w", "utf-8")
+handle = codecs.open('jobs.html', "w", "utf-8")
 handle.write(str(data))
 handle.close()
